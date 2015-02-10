d2d2c6e8300318bc8ef90c012455ebe1d2679d5d
Dagmar Timler <dagmar@cell-life.org>
2015-02-10 15:11:08 +0200

    load latest form instead of just first match

--- src/main/java/org/celllife/hpv/LoadHPVFormTask.java
+++ src/main/java/org/celllife/hpv/LoadHPVFormTask.java
@@ -22,17 +22,20 @@ public class LoadHPVFormTask extends AsyncTask<String, Integer, Uri> {
         
         // find the latest form with the specified key (formPrefix)
         long formId = -1;
+        long formDate = -1;
         String sortOrder = FormsColumns.DISPLAY_NAME + " DESC, " + FormsColumns.JR_VERSION + " DESC";
         Cursor c = context.getContentResolver().query(FormsColumns.CONTENT_URI, null, null, null, sortOrder);
         try {
             if (c.moveToFirst()) {
                 do {
                     int displayNameIndex = c.getColumnIndexOrThrow(FormsColumns.DISPLAY_NAME);
-                    String displayName = c.getString(displayNameIndex);
-                    if (displayName.startsWith(formPrefix)) {
+                    int dateIndex = c.getColumnIndexOrThrow(FormsColumns.DATE);
+                    String thisDisplayName = c.getString(displayNameIndex);
+                    long thisFormDate = c.getLong(dateIndex);
+                    if (thisDisplayName.startsWith(formPrefix) && thisFormDate > formDate) {
                         int idIndex = c.getColumnIndexOrThrow(FormsColumns._ID);
                         formId = c.getLong(idIndex);
-                        break;
+                        formDate = thisFormDate;
                     }
                 } while (c.moveToNext());
             }
